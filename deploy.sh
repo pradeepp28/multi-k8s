2b1babeda2ce7411454d41e4381a9484e2217d6a
Palanisamy, Pradeep <C-Pradeep.Palanisamy@charter.com>
2019-02-08 00:08:12 -0500

    fix travis yml

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t pradeepp28/multi-client:latest -t pradeepp28/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t pradeepp28/multi-client:latest -t pradeepp28/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t pradeepp28/multi-client:latest -t pradeepp28/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t pradeepp28/multi-server:latest -t pradeepp28/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t pradeepp28/multi-worker:latest -t pradeepp28/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push pradeepp28/multi-client:latest
 docker push pradeepp28/multi-server:latest
